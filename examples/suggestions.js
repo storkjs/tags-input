16db945cad2bfa93182b5b254f15ee6f7a3b0a3a
Noam Lin <noam.lin@ebsbrokertec.com>
2016-06-08 19:09:52 +0300

    searching now calls to a user defined function which filters data and passes the suggestion object to a callback

---
+++ examples/suggestions.js
@@ -0,0 +1,29 @@
+var suggestionGroups = [
+  {
+    "displayName": "LPs",
+    "id": "lp",
+    "items": [
+      {"value": "LPLA", "displayName": "LPLA LP"},
+      {"value": "LPRJ", "displayName": "LPRJ LP"},
+      {"value": "LPLB", "displayName": "LPLB LP"}
+    ]
+  },
+  {
+    "displayName": "Currencies",
+    "id": "currencies",
+    "items": [
+      {"value": "USD", "displayName": "US Dollar"},
+      {"value": "EUR", "displayName": "Euro"},
+      {"value": "ILS", "displayName": "IL Shekel"}
+    ]
+  },
+  {
+    "displayName": "Tenors",
+    "id": "tenors",
+    "items": [
+      {"value": "1wk", "displayName": "1 Week"},
+      {"value": "2mth", "displayName": "2 Months"},
+      {"value": "3yr", "displayName": "3 Years"}
+    ]
+  }
+];
