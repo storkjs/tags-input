02b343fa8906de409e917479750e058d5bce7f2b
Noam Lin <noam.lin@ebsbrokertec.com>
2016-06-08 15:27:16 +0300

    build basic dom elements

--- dist/tags-input.js
+++ dist/tags-input.js
@@ -5,7 +5,20 @@
     if (!this.rnd) {
       this.rnd = (Math.floor(Math.random() * 9) + 1) * 1e3 + Date.now() % 1e3;
     }
+    this.chosenTags = [];
+    this.makeList();
     this.tagsInput.classList.add("stork-tags", "stork-tags" + this.rnd);
+  };
+  storkTagsInput.prototype.makeList = function makeList() {
+    var ul = document.createElement("ul");
+    var li = document.createElement("li");
+    var input = document.createElement("input");
+    li.classList.add("input-item");
+    this.ul = ul;
+    this.input = input;
+    li.appendChild(input);
+    ul.appendChild(li);
+    this.tagsInput.appendChild(ul);
   };
   root.storkTagsInput = storkTagsInput;
 })(this);
--- src/tags-input.js
+++ src/tags-input.js
@@ -10,8 +10,26 @@
 		if(!this.rnd) {
 			this.rnd = (Math.floor(Math.random() * 9) + 1) * 1000 + Date.now() % 1000; // random identifier for this grid
 		}
+		this.chosenTags = [];
+
+		this.makeList();
 
 		this.tagsInput.classList.add('stork-tags', 'stork-tags'+this.rnd);
+	};
+
+	storkTagsInput.prototype.makeList = function makeList() {
+		var ul = document.createElement('ul');
+		var li = document.createElement('li');
+		var input = document.createElement('input');
+
+		li.classList.add('input-item');
+
+		this.ul = ul;
+		this.input = input;
+
+		li.appendChild(input);
+		ul.appendChild(li);
+		this.tagsInput.appendChild(ul);
 	};
 
 	root.storkTagsInput = storkTagsInput;
