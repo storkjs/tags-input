b8ecf00a46425575fa46586082cdb73d7e8ebed1
Noam Lin <noam.lin@ebsbrokertec.com>
2016-06-15 13:24:10 +0300

    now using an outer component for shims and polyfills

--- dist/tags-input.js
+++ dist/tags-input.js
@@ -1,32 +1,5 @@
-if (!Number.isInteger) {
-  Number.isInteger = function isInteger(value) {
-    return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
-  };
-}
-
 (function(root) {
   "use strict";
-  var keyboardMap = [ "", "", "", "CANCEL", "", "", "HELP", "", "BACKSPACE", "TAB", "", "", "CLEAR", "ENTER", "ENTER_SPECIAL", "", "SHIFT", "CONTROL", "ALT", "PAUSE", "CAPS_LOCK", "KANA", "EISU", "JUNJA", "FINAL", "HANJA", "", "ESCAPE", "CONVERT", "NONCONVERT", "ACCEPT", "MODECHANGE", "SPACE", "PAGE_UP", "PAGE_DOWN", "END", "HOME", "LEFT", "UP", "RIGHT", "DOWN", "SELECT", "PRINT", "EXECUTE", "PRINTSCREEN", "INSERT", "DELETE", "", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "COLON", "SEMICOLON", "LESS_THAN", "EQUALS", "GREATER_THAN", "QUESTION_MARK", "AT", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "OS_KEY", "", "CONTEXT_MENU", "", "SLEEP", "NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "MULTIPLY", "ADD", "SEPARATOR", "SUBTRACT", "DECIMAL", "DIVIDE", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24", "", "", "", "", "", "", "", "", "NUM_LOCK", "SCROLL_LOCK", "WIN_OEM_FJ_JISHO", "WIN_OEM_FJ_MASSHOU", "WIN_OEM_FJ_TOUROKU", "WIN_OEM_FJ_LOYA", "WIN_OEM_FJ_ROYA", "", "", "", "", "", "", "", "", "", "CIRCUMFLEX", "EXCLAMATION", "DOUBLE_QUOTE", "HASH", "DOLLAR", "PERCENT", "AMPERSAND", "UNDERSCORE", "OPEN_PAREN", "CLOSE_PAREN", "ASTERISK", "PLUS", "PIPE", "HYPHEN_MINUS", "OPEN_CURLY_BRACKET", "CLOSE_CURLY_BRACKET", "TILDE", "", "", "", "", "VOLUME_MUTE", "VOLUME_DOWN", "VOLUME_UP", "", "", "SEMICOLON", "EQUALS", "COMMA", "MINUS", "PERIOD", "SLASH", "BACK_QUOTE", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "OPEN_BRACKET", "BACK_SLASH", "CLOSE_BRACKET", "QUOTE", "", "META", "ALTGR", "", "WIN_ICO_HELP", "WIN_ICO_00", "", "WIN_ICO_CLEAR", "", "", "WIN_OEM_RESET", "WIN_OEM_JUMP", "WIN_OEM_PA1", "WIN_OEM_PA2", "WIN_OEM_PA3", "WIN_OEM_WSCTRL", "WIN_OEM_CUSEL", "WIN_OEM_ATTN", "WIN_OEM_FINISH", "WIN_OEM_COPY", "WIN_OEM_AUTO", "WIN_OEM_ENLW", "WIN_OEM_BACKTAB", "ATTN", "CRSEL", "EXSEL", "EREOF", "PLAY", "ZOOM", "", "PA1", "WIN_OEM_CLEAR", "" ];
-  var getPosition = function getPosition(elm) {
-    var xPos = 0;
-    var yPos = 0;
-    while (elm) {
-      xPos += elm.offsetLeft;
-      yPos += elm.offsetTop;
-      elm = elm.offsetParent;
-    }
-    return {
-      x: xPos,
-      y: yPos
-    };
-  };
-  var getElementIndex = function getElementIndex(elm) {
-    for (var i = 0; i < elm.parentNode.childNodes.length; i++) {
-      if (elm === elm.parentNode.childNodes[i]) {
-        return i;
-      }
-    }
-  };
   var storkTagsInput = function storkTagsInput(options) {
     this.tagsInput = options.element;
     this.suggestionsHandler = options.suggestionsHandler;
@@ -60,7 +33,7 @@ if (!Number.isInteger) {
     dropdownContainer.classList.add("stork-tags-dropdown-container", "stork-tags-dropdown-container" + this.rnd);
     dropdownContainer.setAttribute("tabindex", 0);
     dropdownContainer.style.width = this.tagsInput.offsetWidth + "px";
-    var xy = getPosition(this.tagsInput);
+    var xy = this.tagsInput.getCoordinates();
     dropdownContainer.style.left = xy.x + "px";
     dropdownContainer.style.top = xy.y + this.tagsInput.offsetHeight + 1 + "px";
     this.ul = ul;
@@ -197,6 +170,12 @@ if (!Number.isInteger) {
     li.appendChild(xA);
     li.appendChild(textSpan);
     this.ul.insertBefore(li, this.input.parentNode);
+    var evnt = new CustomEvent("tag-added", {
+      bubbles: true,
+      cancelable: true,
+      detail: this.chosenTags[this.chosenTags.length - 1]
+    });
+    this.tagsInput.dispatchEvent(evnt);
   };
   storkTagsInput.prototype.removeTag = function removeTag(index) {
     if (this.chosenTags[index]) {
@@ -211,7 +190,7 @@ if (!Number.isInteger) {
     var elm = e.target, i = 0;
     do {
       if (elm.tagName.toUpperCase() === "A" && elm.classList.contains("remove")) {
-        var elmIndex = getElementIndex(elm);
+        var elmIndex = elm.index;
         this.removeTag(elmIndex);
         this.focusSearchInput(0);
         return;
@@ -225,7 +204,7 @@ if (!Number.isInteger) {
   };
   storkTagsInput.prototype.onClickFocusTag = function onClickFocusTag(index) {
     if (!Number.isInteger(index)) {
-      index = getElementIndex(index);
+      index = index.index;
     }
     if (Number.isInteger(this.focusedTagIndex)) {
       this.chosenTags[this.focusedTagIndex].elm.classList.remove("focused");
