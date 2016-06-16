93adbdd3ccd9f6ca260c4dd64152a73ff6376118
Noam Lin <noam.lin@ebsbrokertec.com>
2016-06-16 17:40:31 +0300

    removed redundant option

--- test/tags-input.spec.js
+++ test/tags-input.spec.js
@@ -2,10 +2,8 @@
 
 describe('Tags Input', function () {
 	it('construct a tagsInput instance', function () {
-		console.log();
 		var tags = new storkTagsInput({
 			element: document.createElement('div'),
-			suggestionsType: 'ajax',
 			suggestionsHandler: function(cb){},
 			rechooseRemove: true,
 			inputMinWidth: 70
