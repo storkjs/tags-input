7d2922ac866b74a41b07f7786d0022ae98ec59bc
ikarlsson <Ivan.Karlsson@ebsbrokertec.com>
2017-05-10 15:25:34 +0300

    build

--- dist/tags-input.js
+++ dist/tags-input.js
@@ -16,6 +16,7 @@
     this.placeholder = options.placeholder || "";
     this.persistentPlaceholder = options.persistentPlaceholder || false;
     this.multiline = options.multiline || false;
+    this.showGroups = options.showGroups !== undefined ? options.showGroups : true;
     this.textCanvasContext = null;
     this.maxlength = typeof options.maxlength === "number" ? options.maxlength : 50;
     this.maxTags = options.maxTags || 0;
@@ -170,7 +171,9 @@
         item.appendChild(miscElm);
         itemsList.appendChild(item);
       }
-      groupDiv.appendChild(groupHeader);
+      if (this.showGroups) {
+        groupDiv.appendChild(groupHeader);
+      }
       groupDiv.appendChild(itemsList);
       this.dropdownContainer.appendChild(groupDiv);
     }
@@ -281,7 +284,7 @@
     groupSpan.classList.add("group");
     valueSpan.classList.add("value");
     li.appendChild(xA);
-    if (tagObj.groupLabel !== "") {
+    if (tagObj.groupLabel !== "" && this.showGroups) {
       li.appendChild(groupSpan);
     }
     li.appendChild(valueSpan);
